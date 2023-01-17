2e1dc15d0aa0d67ab17bb445d9ea86a8c1d70ebf
akuz0 <91601482+akuz0@users.noreply.github.com>
2023-01-17 11:49:32 -0800

    Update main_test.go
    fixed

--- main_test.go
+++ main_test.go
@@ -17,10 +17,25 @@ var addTests = []addTest{
 	addTest{" ", " "},
 }
 
+var addTestsV2 = []addTest{
+	addTest{"\"Volvo released a new car with the following spec: V6 236HP. It will cost $22647 and going to be sold in New York only\"", "\"Volvo released a new...\""},
+	addTest{"\"Vol releas a new car with the following spec: V6 236HP. It will cost $22647 and going to be sold in New York only\"", "\"Vol releas a new car...\""},
+	addTest{"\"Volvo released\"", "\"Volvo released\""},
+}
+
 func TestShortTitle(t *testing.T) {
 
 	for _, test := range addTests {
 		if output := generateShortTitle(test.arg1); output != test.expected {
+			t.Errorf("Output %q not equal to expected %q", output, test.expected)
+		}
+	}
+}
+
+func TestShortTitleV2(t *testing.T) {
+
+	for _, test := range addTestsV2 {
+		if output := generateShortTitleV2(test.arg1); output != test.expected {
 			t.Errorf("Output %q not equal to expected %q", output, test.expected)
 		}
 	}
