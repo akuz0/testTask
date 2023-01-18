f4f89dc6759601181b40abbd2f10bcada5e826d0
alex <alex@bk.ru>
2023-01-18 07:00:01 -0800

    fixed realisation * deleted generateShortTitleV2 * changed logic for generateShortTitle * changed unit test case

--- main.go
+++ main.go
@@ -3,34 +3,14 @@ package main
 import "strings"
 
 func main() {
-	title := "Volvo released"
+	title := "\"Vol releas a new car for fdfdd\""
 	shortTitle := generateShortTitle(title)
-	print(shortTitle + "\n")
-	title = "\"Vol releas a new car for fdfdd\""
-	shortTitle = generateShortTitleV2(title)
 	print(shortTitle)
 }
 
-func generateShortTitleV2(source string) string {
-	lenCut := 21
-
-	if len(source) <= lenCut {
-		return source
-	}
-	sourceResult := source[0:lenCut]
-	if source[lenCut:lenCut+1] != " " && source[lenCut:lenCut+1] != "!" && source[lenCut:lenCut+1] != "." {
-
-		if strings.Contains(sourceResult, " ") {
-			sourceResult = sourceResult[:strings.LastIndex(sourceResult, " ")] //todo? " "
-		}
-	}
-
-	return sourceResult + "...\""
-}
-
 func generateShortTitle(source string) string {
-	lenCut := 25
-
+	lenCut := 21
+	quotationMark := "\""
 	if len(source) <= lenCut {
 		return source
 	}
@@ -41,6 +21,8 @@ func generateShortTitle(source string) string {
 			sourceResult = sourceResult[:strings.LastIndex(sourceResult, " ")] //todo? " "
 		}
 	}
-
-	return sourceResult + "..."
+	if sourceResult[0:1] != quotationMark {
+		return quotationMark + sourceResult + "..." + quotationMark
+	}
+	return sourceResult + "..." + quotationMark
 }
--- main_test.go
+++ main_test.go
@@ -7,35 +7,19 @@ type addTest struct {
 }
 
 var addTests = []addTest{
-	addTest{"Volvo released a new car with the following spec: V6 236HP. It will cost $22647 and going to be sold in New York only", "Volvo released a new car..."},
-	addTest{"Volvo released", "Volvo released"},
-	addTest{"Volvo released a new cars12 with the following spec: V6 236HP. It will cost $22647 and going to be sold in New York only", "Volvo released a new..."},
-	addTest{"voLVO released a new ololloollo", "voLVO released a new..."},
-	addTest{"voLVO released a new #############", "voLVO released a new..."},
-	addTest{"$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$", "$$$$$$$$$$$$$$$$$$$$$$$$$..."},
-	addTest{"", ""},
-	addTest{" ", " "},
-}
-
-var addTestsV2 = []addTest{
 	addTest{"\"Volvo released a new car with the following spec: V6 236HP. It will cost $22647 and going to be sold in New York only\"", "\"Volvo released a new...\""},
 	addTest{"\"Vol releas a new car with the following spec: V6 236HP. It will cost $22647 and going to be sold in New York only\"", "\"Vol releas a new car...\""},
+	addTest{"Volvo released a new car with the following spec: V6 236HP. It will cost $22647 and going to be sold in New York only", "\"Volvo released a new...\""},
 	addTest{"\"Volvo released\"", "\"Volvo released\""},
+	addTest{"voLVO released a new #############", "\"voLVO released a new...\""},
+	addTest{"", ""},
+	addTest{" ", " "},
 }
 
 func TestShortTitle(t *testing.T) {
 
 	for _, test := range addTests {
 		if output := generateShortTitle(test.arg1); output != test.expected {
-			t.Errorf("Output %q not equal to expected %q", output, test.expected)
-		}
-	}
-}
-
-func TestShortTitleV2(t *testing.T) {
-
-	for _, test := range addTestsV2 {
-		if output := generateShortTitleV2(test.arg1); output != test.expected {
 			t.Errorf("Output %q not equal to expected %q", output, test.expected)
 		}
 	}
